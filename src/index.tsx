1504e4c3767503f351b5324bfae7650943edfb3b
Kamensky124 <kamensky24@gmail.com>
2023-02-03 09:45:11 +0700

    blank project for exams

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {useState} from "react";
+
+function ColorButton() {
+    const [isColored, setIsColored] = useState<boolean>(false)
+    return (
+        <button
+            style={{ backgroundColor: `${ isColored === true ? "red": ""}`}}
+            onClick={()=>setIsColored(true)}
+        >
+            Меняю цвет по клику
+        </button>
+    )
+}
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <ColorButton/>
 );
 
 // If you want to start measuring performance in your app, pass a function
