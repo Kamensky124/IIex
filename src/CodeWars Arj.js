d40781646bf5c85dc7b82b04da834ff41e68bcbd
Kamensky Andrey <kamensky24@gmail.com>
2023-09-13 12:19:46 +0400

    solved 1 kiy

--- src/CodeWars Arj.js
+++ src/CodeWars Arj.js
@@ -1844,3 +1844,29 @@ function twoOldestAges(ages) {
   }
 
 console.log(twoOldestAges([1, 3, 10, 0]));
+    // Testing 1-2-3
+    // Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+    // Write a function which takes a list of strings and returns each line prepended by the correct number.
+    //     The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+    //     Examples: (Input --> Output)
+    //     [] --> []
+    //     ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+    const number=function(array){
+        return array.map((element, index) => `${index + 1}: ${element}`);
+    }
+
+    var number2 = function(arr) {
+        var newArr = [];
+        if (arr.length === 0) {
+            return [];
+        } else {
+            for (var i = 0, len = arr.length; i < len; i += 1) {
+                newArr.push((i + 1) + ': ' + arr[i]);
+            }
+        }
+        return newArr;
+    }
+
+    console.log(number([]));
+    console.log(number(["a", "b", "c"]));
